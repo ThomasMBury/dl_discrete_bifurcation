5778d9726e4f3d9459bf984bcd921669aeb8ef42
Thomas Bury <tombury182@gmail.com>
2023-06-26 21:41:36 -0400

    compute ews for different alpha values

--- revisions/fig_test_fox_params/compute_ews.py
+++ revisions/fig_test_fox_params/compute_ews.py
@@ -46,14 +46,17 @@ print('TF models loaded')
 
 # Load in bifurcation values
 df_bif_vals_alpha = pd.read_csv('output/df_bifvalues_alpha.csv')
-df_bif_vals_scale = pd.read_csv('output/df_bifvlaues_scalup.csv')
+df_bif_vals_scale = pd.read_csv('output/df_bifvalues_scaleup.csv')
 
+alpha_vals = df_bif_vals_alpha['alpha'].values
+scale_vals = df_bif_vals_scale['scale_up'].values
 
-
+model_sims = 5
+id_vals = np.arange(int(model_sims/5)) # number of simulations at each combo of rof and sigma
 
 
 #--------------
-# forced trajectories
+# forced and null trajectories at different alpha values
 #---------------
 print('Simulate forced trajectories and compute EWS')
 list_ktau_forced = []
@@ -61,56 +64,76 @@ list_dl_forced = []
 list_ktau_null = []
 list_dl_null = []
 
+# Model parameters
+A = 88
+B = 122
+C = 40
+D = 28
+tau = 180
+alpha_vals = [0, 0.1, 0.2, 0.3, 0.4]
 
-for rof in rof_vals:
-    for sigma in sigma_vals:
-        for id_val in id_vals:
-         
-            s_forced, transition, s_null = funs.sim_rate_forcing(sigma, rof)
-            
-            # Compute EWS for forced trajectory
-            ts = ewstools.TimeSeries(s_forced, transition=transition)
-            ts.detrend(method='Lowess', span=span)
-            ts.compute_var(rolling_window=rw)
-            ts.compute_auto(rolling_window=rw, lag=1)
-            ts.compute_ktau(tmin=0, tmax=transition*eval_pt)
-            dic_ktau = ts.ktau
-            dic_ktau['sigma'] = sigma
-            dic_ktau['rof'] = rof
-            dic_ktau['id'] = id_val
-            list_ktau_forced.append(dic_ktau)
-            
-            # Get DL predictions for forced trajectory
-            ts.apply_classifier(m1, tmin=0, tmax=transition*eval_pt, name='m1', verbose=0)
-            ts.apply_classifier(m2, tmin=0, tmax=transition*eval_pt, name='m2', verbose=0)
-            df_dl_preds = ts.dl_preds.groupby('time').mean(numeric_only=True) # use mean DL pred
-            df_dl_preds['sigma'] = sigma
-            df_dl_preds['rof'] = rof
-            df_dl_preds['id'] = id_val
-            list_dl_forced.append(df_dl_preds)
+D0 = 200
+M0 = 1
+
+sigma = 0.05
+
+for idx, alpha in enumerate(alpha_vals):
+    bif_val = df_bif_vals_alpha[idx]
+    
+    Tstart = 300
+    Tcrit = 200
+    Tfinal = bif_val
+    niter = 500
+    Tvals = np.linspace(Tstart, Tfinal, niter)    
+    
+    for id_val in id_vals:
         
-            # Compute EWS for null trajectory
-            ts = ewstools.TimeSeries(s_null)
-            ts.detrend(method='Lowess', span=span)
-            ts.compute_var(rolling_window=rw)
-            ts.compute_auto(rolling_window=rw, lag=1)
-            ts.compute_ktau(tmin=0, tmax=transition*eval_pt)
-            dic_ktau = ts.ktau
-            dic_ktau['sigma'] = sigma
-            dic_ktau['rof'] = rof
-            dic_ktau['id'] = id_val
-            list_ktau_null.append(ts.ktau)
+
+        # Simulate forced trajectory up to bifurcation
+        s_forced = funs.simulate_model(A, B, C, D, tau, alpha, M0, D0, Tvals, sigma, sigma)
+        
+        # Simulate a null trajectory up to bifurcation
+        s_null = funs.simulate_model(A, B, C, D, tau, alpha, M0, D0, [Tstart]*niter, sigma, sigma)
+                
+        # Compute EWS for forced trajectory
+        ts = ewstools.TimeSeries(s_forced)
+        ts.detrend(method='Lowess', span=span)
+        ts.compute_var(rolling_window=rw)
+        ts.compute_auto(rolling_window=rw, lag=1)
+        ts.compute_ktau(tmin=0, tmax=niter-1)
+        dic_ktau = ts.ktau
+        dic_ktau['alpha'] = alpha
+        dic_ktau['id'] = id_val
+        list_ktau_forced.append(dic_ktau)
+        
+        # Get DL predictions for forced trajectory
+        ts.apply_classifier(m1, tmin=0, tmax=niter-1, name='m1', verbose=0)
+        ts.apply_classifier(m2, tmin=0, tmax=niter-1, name='m2', verbose=0)
+        df_dl_preds = ts.dl_preds.groupby('time').mean(numeric_only=True) # use mean DL pred
+        df_dl_preds['alpha'] = alpha
+        df_dl_preds['id'] = id_val
+        list_dl_forced.append(df_dl_preds)
+    
+        # Compute EWS for null trajectory
+        ts = ewstools.TimeSeries(s_null)
+        ts.detrend(method='Lowess', span=span)
+        ts.compute_var(rolling_window=rw)
+        ts.compute_auto(rolling_window=rw, lag=1)
+        ts.compute_ktau(tmin=0, tmax=niter-1)
+        dic_ktau = ts.ktau
+        dic_ktau['alpha'] = alpha
+        dic_ktau['id'] = id_val
+        list_ktau_null.append(ts.ktau)
+        
+        # Get DL predictions for null
+        ts.apply_classifier(m1, tmin=0, tmax=niter-1, name='m1', verbose=0)
+        ts.apply_classifier(m2, tmin=0, tmax=niter-1, name='m2', verbose=0)
+        df_dl_preds = ts.dl_preds.groupby('time').mean(numeric_only=True) # use mean DL pred
+        df_dl_preds['alpha'] = alpha
+        df_dl_preds['id'] = id_val
+        list_dl_null.append(df_dl_preds)
             
-            # Get DL predictions for null
-            ts.apply_classifier(m1, tmin=0, tmax=transition*eval_pt, name='m1', verbose=0)
-            ts.apply_classifier(m2, tmin=0, tmax=transition*eval_pt, name='m2', verbose=0)
-            df_dl_preds = ts.dl_preds.groupby('time').mean(numeric_only=True) # use mean DL pred
-            df_dl_preds['sigma'] = sigma
-            df_dl_preds['rof'] = rof
-            df_dl_preds['id'] = id_val
-            list_dl_null.append(df_dl_preds)
-            
-    print('Complete for rof={}'.format(rof))
+    print('Complete for alpha={}'.format(alpha))
 
 df_ktau_forced = pd.DataFrame(list_ktau_forced)
 df_dl_forced = pd.concat(list_dl_forced)
